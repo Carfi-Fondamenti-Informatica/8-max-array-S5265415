65cf9e03fe236812adb0a83e95eeeedfdb4e631a
S5265415 <101595525+S5265415@users.noreply.github.com>
2022-05-07 16:10:37 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  int b=0;
+  cin>> b;
+  float numeri[b];
+  for(int i=0; i<b; i++){
+    cin>> numeri[i];
+  }
+  cout<< max (numeri,b)
   return 0;
 }
